705c280f2d10d8740bb96edc9997999cbb9ad916
Maksim Novichenko <maksim.novichenko@sequra.es>
2020-09-03 11:46:08 +0200

    Bundle gem

---
+++ lib/sequra/style.rb
@@ -0,0 +1,8 @@
+require "sequra/style/version"
+
+module Sequra
+  module Style
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sequra/style/version.rb
@@ -0,0 +1,5 @@
+module Sequra
+  module Style
+    VERSION = "0.1.0"
+  end
+end
